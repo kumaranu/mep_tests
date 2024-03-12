7cb7ac950c7030aa81a1618ac9b655098478fa33
kumaranu <anukumar94663@gmail.com>
2024-03-12 13:44:04 -0700

    Changed the GPU call to CPU

--- src/calcs.py
+++ src/calcs.py
@@ -4,8 +4,10 @@ from mace.calculators import MACECalculator
 def calc():
 
     mlcalculator = MACECalculator(
-        model_paths='/global/home/users/kumaranu/Documents/gpu_jobs/MACE_model.model',
-        device='cuda',
+        #model_paths='/global/home/users/kumaranu/Documents/gpu_jobs/MACE_model.model',
+        model_paths='/global/home/users/kumaranu/Documents/gpu_jobs/MACE_model_cpu.model',
+        #device='cuda',
+        device='cpu',
         default_dtype="float64",
     )
     return mlcalculator
