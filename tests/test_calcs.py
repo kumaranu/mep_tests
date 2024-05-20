6ea09898f5af2e4e2bf2666a4cbc13c18b780fb0
kumaranu <anukumar94663@gmail.com>
2024-05-20 12:38:20 -0700

    Auto stash before merge of "main" and "origin/main"
    added newtonnet calc tests. So far, energy and forces are fine in the tests but Hessian is returned as a zero vector (3D matrix).

--- tests/test_calcs.py
+++ tests/test_calcs.py
@@ -1,7 +1,9 @@
 import pytest
+from ase.io import read
 from calcs import load_config, calc_mace, calc
 from mace.calculators import MACECalculator
 from newtonnet.utils.ase_interface import MLAseCalculator
+#from conftest import setup_test_environment
 
 
 def test_load_config():
@@ -19,11 +21,38 @@ def test_load_config():
         assert 'config_path' in config['newtonnet'], "'newtonnet' should have a 'config_path' key"
 
 
-def test_calc_mace():
+def test_calc_mace(setup_test_environment):
     ml_calculator = calc_mace()
     assert isinstance(ml_calculator, MACECalculator)
 
 
-def test_calc():
+def test_calc(setup_test_environment):
     ml_calculator = calc()
     assert isinstance(ml_calculator, MLAseCalculator)
+
+    logdir, xyz_r_p = setup_test_environment
+    print('xyz_r_p:', xyz_r_p)
+    atoms_object = read(xyz_r_p)
+    ml_calculator.calculate(atoms_object)
+
+    assert ml_calculator.results['energy'] == pytest.approx(
+        -20.7079663,
+        abs=1e-5,
+    )
+    "error in energy"
+
+    assert ml_calculator.results['forces'][0, 0] == pytest.approx(
+        5.6846989,
+        abs=1e-5,
+    )
+    "error in forces"
+
+    '''
+    !!!!! HESSIAN TESTS FAIL RIGHT NOW !!!!!
+    !!!!! NEED TO TALK TO Eric !!!!!
+    assert sum(sum(sum(ml_calculator.results['hessian']))) == pytest.approx(
+        5.6846989,
+        abs=1e-5,
+    )
+    "error in Hessian"
+    '''
